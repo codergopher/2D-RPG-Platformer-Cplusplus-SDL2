375cde81b067f3f0743f85ebf7ff522f8d1bfc98
codergopher <57048930+codergopher@users.noreply.github.com>
2020-08-04 12:30:04 -0600

    Add files via upload

---
+++ Part 8/src/main.cpp
@@ -0,0 +1,70 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#include <vector>
+
+#include "RenderWindow.hpp"
+#include "Entity.hpp"
+
+
+int main(int argc, char* args[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+		std::cout << "HEY.. SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
+
+	if (!(IMG_Init(IMG_INIT_PNG)))
+		std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
+
+	RenderWindow window("GAME v1.0", 1280, 720);
+
+	SDL_Texture* grassTexture = window.loadTexture("res/gfx/ground_grass_1.png");
+
+    // Entity entities[4] = {Entity(0, 0, grassTexture),
+    //                       Entity(30, 0, grassTexture),
+    //                       Entity(30, 30, grassTexture),
+    //                       Entity(30, 60, grassTexture)};
+
+    std::vector<Entity> entitiees = {Entity(0, 0, grassTexture),
+                         			 Entity(30, 0, grassTexture),
+                          			 Entity(30, 30, grassTexture),
+                          			 Entity(30, 60, grassTexture)};
+    {
+	    Entity wilson(100, 50, grassTexture);
+
+	    entitiees.push_back(wilson);
+	    
+	}
+
+
+
+	bool gameRunning = true;
+
+	SDL_Event event;
+
+	while (gameRunning)
+	{
+		// Get our controls and events
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+				gameRunning = false;
+		}
+
+		window.clear();
+		
+
+		for (Entity& e : entitiees)
+		{ 
+			window.render(e);
+		}
+
+
+		window.display();
+
+	}
+
+	window.cleanUp();
+	SDL_Quit();
+
+	return 0;
+}
---
+++ Part 8/src/renderwindow.cpp
@@ -0,0 +1,62 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
+#include "RenderWindow.hpp"
+#include "Entity.hpp"
+
+RenderWindow::RenderWindow(const char* p_title, int p_w, int p_h)
+	:window(NULL), renderer(NULL)
+{
+	window = SDL_CreateWindow(p_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, p_w, p_h, SDL_WINDOW_SHOWN);
+
+	if (window == NULL)
+	{
+		std::cout << "Window failed to init. Error: " << SDL_GetError() << std::endl;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
+
+SDL_Texture* RenderWindow::loadTexture(const char* p_filePath)
+{
+	SDL_Texture* texture = NULL;
+	texture = IMG_LoadTexture(renderer, p_filePath);
+
+	if (texture == NULL)
+		std::cout << "Failed to load texture. Error: " << SDL_GetError() << std::endl;
+
+	return texture;
+}
+
+void RenderWindow::cleanUp()
+{
+	SDL_DestroyWindow(window);
+}
+
+void RenderWindow::clear()
+{
+	SDL_RenderClear(renderer);
+}
+
+void RenderWindow::render(Entity& p_entity)
+{
+	SDL_Rect src; 
+	src.x = p_entity.getCurrentFrame().x;
+	src.y = p_entity.getCurrentFrame().y;
+	src.w = p_entity.getCurrentFrame().w;
+	src.h = p_entity.getCurrentFrame().h;
+
+	SDL_Rect dst;
+	dst.x = p_entity.getX() * 4;
+	dst.y = p_entity.getY() * 4;
+	dst.w = p_entity.getCurrentFrame().w * 4;
+	dst.h = p_entity.getCurrentFrame().h * 4;
+
+	SDL_RenderCopy(renderer, p_entity.getTex(), &src, &dst);
+}
+
+void RenderWindow::display()
+{
+	SDL_RenderPresent(renderer);
+}
