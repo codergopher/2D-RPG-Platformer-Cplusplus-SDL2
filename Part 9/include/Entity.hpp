90b620e5654328ff5eef2dcf6a48cd7eb457a06d
codergopher <57048930+codergopher@users.noreply.github.com>
2020-09-28 18:54:19 -0600

    Add files via upload

---
+++ Part 9/include/Entity.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <Math.hpp>
+
+class Entity
+{
+public:
+	Entity(Vector2f p_pos, SDL_Texture* p_tex);
+	Vector2f& getPos()
+	{
+		return pos;
+	}
+	SDL_Texture* getTex();
+	SDL_Rect getCurrentFrame();
+private:
+	Vector2f pos;
+
+	SDL_Rect currentFrame;
+	SDL_Texture* tex;
+};
